1a925e6a26687e5445cac50a0d6ce53a1224a090
Matthew Shaw <mattalexshaw@gmail.com>
2023-03-31 19:45:10 +0100

    fix image dimensions on load

--- src/model/BGImage.cpp
+++ src/model/BGImage.cpp
@@ -9,11 +9,10 @@
 
 #include <model/BGImage.h>
 
-
 BGImage::BGImage(std::shared_ptr<Mesh> mesh, std::shared_ptr<Texture> texture) : Rect(mesh), m_texture(texture)
 {
     if (m_texture->IsValid())
-        m_model->SetScale(glm::vec2(m_texture->width / DEFAULT_PIXELS_PER_UNIT, m_texture->width / DEFAULT_PIXELS_PER_UNIT));
+        m_model->SetScale(glm::vec2(m_texture->height / DEFAULT_PIXELS_PER_UNIT, m_texture->width / DEFAULT_PIXELS_PER_UNIT));
 }
 
 void BGImage::Draw(Shader &shader)
@@ -48,7 +47,7 @@ void BGImage::SetImage(std::shared_ptr<Texture> texture)
         // Resize the incoming image to match the width of the current image
         float hSize = m_texture->width * m_model->GetScale().x;
         m_texture = texture;
-        m_model->SetScale(glm::vec2(hSize / texture->width, hSize / texture->height));
+        m_model->SetScale(glm::vec2(hSize / texture->height, hSize / texture->width));
     }
     else
     {
@@ -64,4 +63,3 @@ void BGImage::SetTint(glm::vec4 colour)
 
 bool BGImage::GetLockRatio() { return m_lockRatio; }
 void BGImage::SetLockRatio(bool lockRatio) { m_lockRatio = lockRatio; }
-
